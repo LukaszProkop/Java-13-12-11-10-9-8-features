3de0d2e064df2807f931e95fbf231cf57238b7d1
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-07-21 00:30:29 +0200

    var with lambda

---
+++ src/com/modernjava/javaimprovments/VarWithLambdaExample.java
@@ -0,0 +1,24 @@
+package com.modernjava.javaimprovments;
+
+import com.modernjava.functiolanPrograming.Instructor;
+import com.modernjava.functiolanPrograming.Instructors;
+
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+public class VarWithLambdaExample {
+    public static void main(String[] args) {
+        var instructors = Instructors.getAll();
+        Predicate<Instructor> experiencePredicate = (var s) -> s.getYearsOfExperience() > 10;
+        instructors.forEach(instructor -> {
+            if (experiencePredicate.test(instructor)) {
+                var result = instructor.getName();
+                System.out.println("result = " + result);
+            }
+        });
+
+        BiFunction<Integer, Integer, Integer> sum = (var x, var y) -> x + y;
+        System.out.println("sum.apply(2,4) = " + sum.apply(2, 4));
+
+    }
+}
